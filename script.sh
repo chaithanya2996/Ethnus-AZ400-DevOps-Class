a1fb657d92d8a66acb4abb6c2f9a2e7b0bb8999a
Chaithanya G <chaithanya2996@gmail.com>
2026-06-17 19:48:22 +0530

    modified and added pipelines

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script is used to run the application in development mode.
+echo "Running the application in development mode..."
+
+echo "Starting the application..."
+
+echo "Application is running. Press Ctrl+C to stop."
+
+echo "Application stopped."
